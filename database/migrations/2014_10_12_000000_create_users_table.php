8da9fb789ac9c39bdee89a7d619cb2cb80ffaeb6
fily666 <wilmarfeliper@gmail.com>
2022-07-17 18:14:50 -0500

    Creacion de roles de usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,24 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
         Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->unsignedBigInteger('role_id')->default('2');
+            $table->foreign('role_id')->references('id')->on('roles');
+
             $table->rememberToken();
             $table->timestamps();
         });
@@ -32,5 +44,7 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+
+        Schema::dropIfExists('roles');
     }
 }
